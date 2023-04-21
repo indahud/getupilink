7ff33f33b6b84c2a8d08fd9872030a05f575edf2
संकेत <47686324+indahud@users.noreply.github.com>
2023-04-21 10:10:39 +0530

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -176,7 +176,7 @@ const UpiForm = () => {
                   <input 
                     value={upiLink.url}
                     className="mr-2 w-full px-3 text-gray-700 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                  <button onClick={copyLink} className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <button onClick={copyLink} className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none">
                   {copyMessage}
                   </button>
                 </div>
