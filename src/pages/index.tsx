b94aa67a6673b44d1793191462f4753413b54f6a
संकेत <47686324+indahud@users.noreply.github.com>
2023-04-28 17:51:00 +0530

    Fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -151,7 +151,9 @@ const UpiForm = () => {
                                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                               />
                               {meta.error && meta.touched && (
-                                <div>{meta.error}</div>
+                                <div className="text-pink-500">
+                                  {meta.error}
+                                </div>
                               )}
                             </div>
                           )}
@@ -173,18 +175,22 @@ const UpiForm = () => {
             ) : (
               <>
                 <div className="flex items-center">
-                  <input 
+                  <input
                     value={upiLink.url}
-                    className="mr-2 w-full px-3 text-gray-700 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                  <button onClick={copyLink} className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none">
-                  {copyMessage}
+                    className="mr-2 w-full px-3 text-gray-700 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                  <button
+                    onClick={copyLink}
+                    className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
+                  >
+                    {copyMessage}
                   </button>
                 </div>
 
                 <div>
                   <button
                     type="submit"
-                    onClick={redirectToPage} 
+                    onClick={redirectToPage}
                     className="w-full mt-6 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Preview
--- src/pages/upi/[ID].tsx
+++ src/pages/upi/[ID].tsx
@@ -1,19 +1,52 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import QRCode from "react-qr-code";
-
-
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import QRCode from 'react-qr-code';
 
 const UpiQR: NextPage = () => {
-    const router = useRouter();
-    const { ID, am } = router.query;
-    const paymentlink = `upi://pay?pn=withUpigen&pa=${ID}&cu=INR${am ? am : ''}`
-    return (
-        <>
-            <h1>QR Page</h1>
-            <QRCode value={paymentlink} size={200} />
-        </>
-    )
-}
+  const router = useRouter();
+  const { ID, am } = router.query;
+  const paymentlink = `upi://pay?pn=withUpigen&pa=${ID}&cu=INR${`&am=${
+    am ? am : ''
+  }`}`;
+
+  const redirectToUpi = () => {
+    router.push(paymentlink);
+  };
+  return (
+    <>
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 sm:rounded-lg sm:px-10 border border-gray-300 shadow">
+            <div className="mb-4">
+              <p className='flex items-center justify-center text-2xl tracking-tight text-gray-700'>
+                You are paying {am ? `INR ${am}` : ''}
+              </p>
+              <p className='flex items-center mt-1 justify-center text-2xl tracking-tight text-gray-700'>
+                to 👉 <span className='ml-2 font-bold'>{ID}</span>
+              </p>
+            </div>
+            <div className="flex items-center justify-center">
+              <QRCode
+                value={paymentlink}
+                size={400}
+                className="border border-gray-300 px-10 py-10 rounded-md"
+              />
+            </div>
+            <div className="flex items-center justify-center mt-2 text-gray-900 text-lg">
+              <p>Scan this QR with any UPI ID</p>
+            </div>
+          </div>
+          <div>
+            <div className="w-full mt-6 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <a href={paymentlink} target="_blank" rel="noopener noreferrer">
+                Pay
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default UpiQR;
